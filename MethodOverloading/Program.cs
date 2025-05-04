975e7e7ef87de18a8d76ebc22503241859d1fdd4
sreynolds09 <sara.reynolds.clan@gmail.com>
2025-05-04 13:31:47 -0400

    initial commit

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,36 @@
 {
     public class Program
     {
+        public static int Add(int numOne, int numTwo)
+        {
+            return numOne + numTwo;
+        }
+
+        public static decimal Add(decimal numOne, decimal numTwo)
+        {
+            return numOne + numTwo;
+        }
+        public static string Add(int numOne, int numTwo, bool isMoney)
+        {
+            var sum = numOne + numTwo;
+
+            if (isMoney && sum > 1)
+            {
+                return $"{sum} Dollars";
+            }
+            else if (isMoney && sum == 1)
+            {
+                return $"{sum} Dollar";
+            }
+            else
+            {
+                return sum.ToString();
+            }   
+        }
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Add(numOne: 0, numTwo: 1, isMoney: true));
         }
     }
 }
--- MethodOverloading/obj/Debug/net9.0/MethodOverloading.AssemblyInfo.cs
+++ MethodOverloading/obj/Debug/net9.0/MethodOverloading.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MethodOverloading")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eddcfa4da02f5351bf45b7a3c534caeddb2b5239")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39be9af47ee764eb14069c73a057eeb26ad92017")]
 [assembly: System.Reflection.AssemblyProductAttribute("MethodOverloading")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MethodOverloading")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
