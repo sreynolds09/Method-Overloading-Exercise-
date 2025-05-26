64a723fe34e3ec2d6a3b1061b3893c3a4d3e4220
sreynolds09 <sara.reynolds.clan@gmail.com>
2025-05-26 12:01:14 -0400

    Added Methods to Main

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,7 +1,28 @@
-﻿namespace MethodOverloading
+﻿using System.Net.Sockets;
+
+namespace MethodOverloading
 {
     public class Program
     {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Add(numOne: 2, numTwo: 1, isMoney: true));
+            
+            var x = 2;
+            var y = 55;
+			
+            var answer = Add(x, y);
+			
+            var a = 3.40m;
+            var b = 45.9m;
+			
+            var decimalAnswer = Add(a,b);
+			
+            var thirdAnswer = Add(0, 0, true);
+			
+            Console.WriteLine($"int add: {answer} decimal add: {decimalAnswer}");
+            Console.WriteLine(thirdAnswer);
+        }
         public static int Add(int numOne, int numTwo)
         {
             return numOne + numTwo;
@@ -26,12 +47,11 @@
             else
             {
                 return sum.ToString();
-            }   
+            }
+
+            
         }
+   
         
-        static void Main(string[] args)
-        {
-            Console.WriteLine(Add(numOne: 0, numTwo: 1, isMoney: true));
-        }
     }
 }
--- MethodOverloading/obj/Debug/net9.0/MethodOverloading.AssemblyInfo.cs
+++ MethodOverloading/obj/Debug/net9.0/MethodOverloading.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MethodOverloading")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39be9af47ee764eb14069c73a057eeb26ad92017")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+975e7e7ef87de18a8d76ebc22503241859d1fdd4")]
 [assembly: System.Reflection.AssemblyProductAttribute("MethodOverloading")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MethodOverloading")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
